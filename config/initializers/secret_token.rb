81519197dc99094adf74613347beb72c62b3cefc
rubycs <rubycs@clair.ro>
2010-11-29 18:44:02 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Couch::Application.config.secret_token = 'd7f8a626c911f8e6b13466f6dcb4d423fb26870ebd2bb22c63a79df396437e3c87fc04e3417efb306f20afdbc44339aa2fccf89c79a9a76bd913deeedc4b63ae'
